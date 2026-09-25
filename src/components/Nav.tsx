212327aa16b0087b9a4f895eeb5a3d5151f2d03b
Tsega Anberber <tsegaanberebr86@gmail.com>
2026-09-25 15:00:30 +0300

    fix 1

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 
-const assetPathPrefix = 'public/assets';
-const imgOriginTech2 = "public/assets/d2b8a.png";
+const assetPathPrefix = '/assets';
+const imgOriginTech2 = "/assets/d2b8a.png";
 const imgEllipse = `${assetPathPrefix}/ef410.svg`;
 
 const links = [
